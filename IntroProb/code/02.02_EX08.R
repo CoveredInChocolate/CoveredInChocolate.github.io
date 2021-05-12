ad6a99d7699e56863b21bea09168d9fa5cf51033
Sindre Froyn <sindre.froyn@gmail.com>
2021-05-12 12:18:28 +0200

    INPROB: Fixed coloring of plots

--- IntroProb/code/02.02_EX08.R
+++ IntroProb/code/02.02_EX08.R
@@ -46,61 +46,61 @@ pi*(1/2)**2
 B = runif(NSIMS)
 C = runif(NSIMS)
 
-par(mfrow=c(3,3))
 png(filename = "~/GITHUB/CoveredInChocolate.github.io/IntroProb/img/02.02_Ex8_COMP.png",
     width = 820, height=650)
+par(mfrow=c(3,3))
 
 # (a)
 ind = B + C < 0.5
-plot(B[!ind], C[!ind], pch=16, col="lightgray", xlim=c(0, 1), ylim=c(0, 1),
+plot(B[!ind], C[!ind], pch=16, col="white", xlim=c(0, 1), ylim=c(0, 1),
      main="(a)", xlab="B", ylab="C")
-points(B[ind], C[ind], pch=16, col="darkgray")
+points(B[ind], C[ind], pch=16, col="lightgray")
 
 # (b)
 ind = B*C < 0.5
-plot(B[!ind], C[!ind], pch=16, col="lightgray", xlim=c(0, 1), ylim=c(0, 1),
+plot(B[!ind], C[!ind], pch=16, col="white", xlim=c(0, 1), ylim=c(0, 1),
      main="(b)", xlab="B", ylab="C")
-points(B[ind], C[ind], pch=16, col="darkgray")
+points(B[ind], C[ind], pch=16, col="lightgray")
 
 # (c)
 ind = abs(B - C) < 0.5
-plot(B[!ind], C[!ind], pch=16, col="lightgray", xlim=c(0, 1), ylim=c(0, 1),
+plot(B[!ind], C[!ind], pch=16, col="white", xlim=c(0, 1), ylim=c(0, 1),
      main="(c)", xlab="B", ylab="C")
-points(B[ind], C[ind], pch=16, col="darkgray")
+points(B[ind], C[ind], pch=16, col="lightgray")
 
 # (d)
 ind = pmax(B, C) < 0.5
-plot(B[!ind], C[!ind], pch=16, col="lightgray", xlim=c(0, 1), ylim=c(0, 1),
+plot(B[!ind], C[!ind], pch=16, col="white", xlim=c(0, 1), ylim=c(0, 1),
      main="(d)", xlab="B", ylab="C")
-points(B[ind], C[ind], pch=16, col="darkgray")
+points(B[ind], C[ind], pch=16, col="lightgray")
 
 # (e)
 ind = pmin(B, C) < 0.5
-plot(B[!ind], C[!ind], pch=16, col="lightgray", xlim=c(0, 1), ylim=c(0, 1),
+plot(B[!ind], C[!ind], pch=16, col="white", xlim=c(0, 1), ylim=c(0, 1),
      main="(e)", xlab="B", ylab="C")
-points(B[ind], C[ind], pch=16, col="darkgray")
+points(B[ind], C[ind], pch=16, col="lightgray")
 
 # (f)
 ind = B < 0.5 & 1 - C < 0.5
-plot(B[!ind], C[!ind], pch=16, col="lightgray", xlim=c(0, 1), ylim=c(0, 1),
+plot(B[!ind], C[!ind], pch=16, col="white", xlim=c(0, 1), ylim=c(0, 1),
      main="(f)", xlab="B", ylab="C")
-points(B[ind], C[ind], pch=16, col="darkgray")
+points(B[ind], C[ind], pch=16, col="lightgray")
 
 # (g)
 ind = (B < 0.5 & 1 - C < 0.5) & (abs(B - C) < 0.5)
-plot(B[!ind], C[!ind], pch=16, col="lightgray", xlim=c(0, 1), ylim=c(0, 1),
+plot(B[!ind], C[!ind], pch=16, col="white", xlim=c(0, 1), ylim=c(0, 1),
      main="(g)", xlab="B", ylab="C")
-points(B[ind], C[ind], pch=16, col="darkgray")
+points(B[ind], C[ind], pch=16, col="lightgray")
 
 # (h)
 ind = B**2 + C**2 < 0.5
-plot(B[!ind], C[!ind], pch=16, col="lightgray", xlim=c(0, 1), ylim=c(0, 1),
+plot(B[!ind], C[!ind], pch=16, col="white", xlim=c(0, 1), ylim=c(0, 1),
      main="(h)", xlab="B", ylab="C")
-points(B[ind], C[ind], pch=16, col="darkgray")
+points(B[ind], C[ind], pch=16, col="lightgray")
 
 # (i)
 ind = (B-0.5)**2 + (C-0.5)**2 < 0.25
-plot(B[!ind], C[!ind], pch=16, col="lightgray", xlim=c(0, 1), ylim=c(0, 1),
+plot(B[!ind], C[!ind], pch=16, col="white", xlim=c(0, 1), ylim=c(0, 1),
      main="(i)", xlab="B", ylab="C")
-points(B[ind], C[ind], pch=16, col="darkgray")
+points(B[ind], C[ind], pch=16, col="lightgray")
 dev.off()
