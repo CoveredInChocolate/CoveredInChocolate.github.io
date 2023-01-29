1842c6a0d051b7f05d672587551f8c407fb2332a
Sindre Froyn <sindre.froyn@gmail.com>
2023-01-29 17:50:16 +0100

    Fixing python script

---
+++ LinAlgLay/genmat.py
@@ -0,0 +1,68 @@
+# Python script for generating LaTeX matrices
+import sys
+
+mat = """
+1 0 0
+0 1 0
+0 0 1
+"""
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    ERROR = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
+def TrimWhite(ss):
+    ss = ss.split("\n")
+    # Removing leading and trailing whitespaces
+    ss = ss[1:len(ss)-1]
+    # Trimming rows
+    for k in range(len(ss)):
+        #print(k)
+        # print(ss[k])
+        ss[k] = ss[k].split(" ")
+    return ss
+
+
+def FindDim(ss):
+    numrows = len(ss)
+    numcols = len(ss[0])
+    # Check that all rows have the same dimensions
+    for rw in ss:
+        tmprow = len(rw)
+        if tmprow != numcols:
+            print(f"{bcolors.ERROR}ERROR: col mismatch!{bcolors.ENDC}")
+            sys.exit()
+    # # Print dimensions
+    #print(numrows, "-", numcols)
+    return numrows, numcols
+
+def MatrixToLaTex(ss):
+    mat = TrimWhite(ss)
+    numrow, numcols = FindDim(mat)
+
+    #print(mat)
+
+    print("\\begin{bmatrix}")
+    for i in range(numrow):
+        for j in range(numcols):
+            if j < numcols - 1:
+                print(mat[i][j], end = " & ")
+            elif i == numrow - 1:
+                print(mat[i][j], end = "")
+            else:
+                print(mat[i][j], end = " \\\\ ")
+        print("")
+    print("\\end{bmatrix}")
+
+
+MatrixToLaTex(mat)
+
+
